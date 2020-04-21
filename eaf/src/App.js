30219cb146b6fe8499259767da727b7504ce2c8b
anthonyvigz <anthonyvigz@gmail.com>
2020-04-21 09:52:29 -0400

    initialized website

---
+++ eaf/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mainApp">
+      <Home />
+      <About />
+      <Gallery />
+      <Reviews />
+      <Quote />
+    </div>
+  );
+}
+
+export default App;
